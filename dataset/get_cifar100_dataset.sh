51e2484ac38d2073afe5e3d4df8b3a12240707a7
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2020-05-20 09:42:17 -0400

    code cleanup for merge

--- dataset/get_cifar100_dataset.sh
+++
@@ -1,4 +0,0 @@
-# Get CIFAR10
-wget https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz
-tar -xzvf cifar-100-python.tar.gz
-rm cifar-100-python.tar.gz 
--- tests/dot_product_test.py
+++ tests/dot_product_test.py
@@ -1,18 +1,20 @@
 from neuralteleportation.neuralteleportationmodel import NeuralTeleportationModel
+
 import numpy as np
 import torch
+
+red = "\033[31m"
+reset = "\033[0m"
 
 
 def test_set_weights(network, input_shape=(1, 1, 28, 28)):
     model = NeuralTeleportationModel(network, input_shape)
     w1 = model.get_weights().detach().numpy()
     model = NeuralTeleportationModel(network, input_shape)
-    # w2 = model.get_weights().detach().numpy()
 
     model.set_weights(w1)
     w3 = model.get_weights().detach().numpy()
 
-    # assert not np.allclose(w1, w2)
     assert np.allclose(w1, w3)
 
 
@@ -22,8 +24,6 @@ def test_dot_product(network, input_shape=(1, 1, 28, 28), verbose=False) -> None
     y = torch.rand((1, 1, 10, 10), dtype=torch.float)
     w1 = model.get_weights().detach().numpy()
     sampling_types = ['usual', 'symmetric', 'negative', 'zero']
-    red = "\033[31m"
-    reset = "\033[0m"
 
     loss_func = torch.nn.MSELoss()
 
@@ -35,8 +35,9 @@ def test_dot_product(network, input_shape=(1, 1, 28, 28), verbose=False) -> None
 
         dot_prod = np.dot(grad, (w2 - w1))
 
-        print(f'The result of the scalar product is: {red * (not np.allclose(dot_prod, 0, atol=1e-5))}{dot_prod}'
-              f'{reset} (using {sampling_type} sampling type)')
+        print(f'The result of the scalar product is: '
+              f'{red * (not np.allclose(dot_prod, 0, atol=1e-5))}{dot_prod}'
+              f'{reset} using {sampling_type} sampling type')
 
 
 def test_reset_weights(network, input_shape=(1, 1, 28, 28)):
@@ -46,6 +47,7 @@ def test_reset_weights(network, input_shape=(1, 1, 28, 28)):
     w2 = model.get_weights().detach().numpy()
 
     assert not np.allclose(w1, w2)
+
 
 if __name__ == '__main__':
     import torch.nn as nn
