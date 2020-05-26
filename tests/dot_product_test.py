42dbab1fc27bc15dad4678b74131092462533f55
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2020-05-25 23:41:26 -0400

    adds histograms for random vectore & micro-teleportation scalar products with the gradient

--- tests/dot_product_test.py
+++ tests/dot_product_test.py
@@ -12,16 +12,20 @@ red = "\033[31m"
 reset = "\033[0m"
 
 
-def test_dot_product(network, input_shape=(1, 1, 28, 28)) -> None:
+def test_dot_product(network, input_shape=(1, 1, 28, 28), teleport_iterations=200) -> None:
     """
-    This method tests the scalar produt between the teleporation line and the gradient for nullity
+    This method tests the scalar produt between the teleporation line and the gradient, as well as between a random
+    vector and the gradient for nullity. It then displays the histograms of the calculated scalar products.
 
     Args:
-        network : the model to which we wish to assigne weights
+        network :               the model to which we wish to assigne weights
 
-        input_shape :   the shape of the input
-                        (it will be used by the networkgrapher of the model,
-                        the values is not important for the test at hand)
+        input_shape :           the shape of the input
+                                (it will be used by the networkgrapher of the model,
+                                the values is not important for the test at hand)
+
+        teleport_iterations:    The number of time the network is teleported and the scalar product calculated. An
+                                average is then calculated.
     """
 
     model = NeuralTeleportationModel(network=network, input_shape=input_shape)
@@ -30,25 +34,20 @@ def test_dot_product(network, input_shape=(1, 1, 28, 28)) -> None:
     w1 = model.get_weights().detach().numpy()
     sampling_types = ['usual', 'symmetric', 'negative', 'zero']
 
-    iterations = range (0, 100)
+    iterations = range (0, teleport_iterations)
     loss_func = torch.nn.MSELoss()
 
     grad = model.get_grad(x, y, loss_func, zero_grad=False)
 
     for sampling_type in sampling_types:
-        for pow in range(-6, -5):
+        for pow in range(-5, 2):
             cob = 10 ** pow
             dot_prod_results = []
             rand_dot_prod_results = []
-            for iteration in iterations:
-                # model = NeuralTeleportationModel(network=network, input_shape=input_shape)
+            for _ in iterations:
                 model.set_weights(w1)
                 model.random_teleport(cob_range=cob, sampling_type=sampling_type)
                 w2 = model.get_weights().detach().numpy()
-
-                assert not (w2 == np.inf).any(), \
-                    f'Weights have diverged to infinity after telportation. Consider reducing COB iterative ' \
-                    f'range (for pow in range (-4, <superior limit>)).'
 
                 # Normalized scalar product
                 dot_prod = np.longfloat(np.dot(grad, (w2 - w1))/(np.linalg.norm(grad)*np.linalg.norm((w2 - w1))))
@@ -56,7 +55,8 @@ def test_dot_product(network, input_shape=(1, 1, 28, 28)) -> None:
 
                 # Create a random vector to compare repectives scalar products
                 random_vector = torch.rand(grad.shape, dtype=torch.float)
-                rand_dot_prod = np.longfloat(np.dot(grad, random_vector)/(np.linalg.norm(grad)*np.linalg.norm(random_vector)))
+                rand_dot_prod = np.longfloat(np.dot(grad, random_vector)/
+                                             (np.linalg.norm(grad)*np.linalg.norm(random_vector)))
                 rand_angle = np.degrees(np.arccos(rand_dot_prod))
 
                 # Arbitrary precision threshold for nullity comparison
@@ -65,32 +65,61 @@ def test_dot_product(network, input_shape=(1, 1, 28, 28)) -> None:
                 rand_failed = (not np.allclose(rand_dot_prod, 0, atol=tol))
                 target_angle = 90
 
-                rand_dot_prod_results.append(rand_dot_prod)
                 dot_prod_results.append(dot_prod)
+                rand_dot_prod_results.append(rand_dot_prod)
 
+            dot_prod_results = np.array(dot_prod_results)
+            rand_dot_prod_results = np.array(rand_dot_prod_results)
 
             print(f'The result of the scalar product between the gradient and a micro-teleporation vector is: '
-                  f'{red * failed}{np.round(np.array(dot_prod_results).mean(), abs(int(np.log10(tol))))}',
+                  f'{red * failed}{np.round(dot_prod_results.mean(), abs(int(np.log10(tol))))}',
                   f' (!=0 => FAILED!)' * failed,
                   f'{reset}',
                   f' using {sampling_type} sampling type',
                   f', the angle is {angle}°',
                   f', the delta in angle is {angle - target_angle}°\n',
                   f'The scalar product  between the gradient and a random vector is: ',
-                  f'{red * rand_failed}{np.array(rand_dot_prod_results).mean()}',
+                  f'{red * rand_failed}{rand_dot_prod_results.mean()}',
                   f' (!=0 => FAILED!)' * rand_failed,
                   f'{reset}',
                   f', and the angle is {rand_angle}°',
                   f', the delta in angle is {rand_angle - target_angle}°\n',
                   sep='')
 
-            plt.hist(np.array(dot_prod_results), color='b')
-            plt.title(f'Micro-teleportation: Sampling type: {sampling_type}, cob range: {cob}')
-            plt.show()
+            bins = 'fd'
+            # This conditionnal display is necessary because some sampling type/COB combinations produce such a narrow
+            # distribution for micro-teleportation that pyplot is not able to display them at all
+            if np.log10(rand_dot_prod_results.var() / dot_prod_results.var()) < 3.7:
+                plt.hist(dot_prod_results, color='b', bins=bins)
+                plt.hist(rand_dot_prod_results, color='g', bins=bins)
+                plt.legend(['Micro-teleportation', 'Random vector'])
+                plt.title(f'Sampling type: {sampling_type}, cob range: {cob}\n'
+                          f'{teleport_iterations} iterations')
+                plt.text(dot_prod_results.mean(), plt.ylim()[1]/2,
+                         f'Micro: $\\mu={dot_prod_results.mean():.3e}$,\n'
+                         f'$\\sigma={dot_prod_results.var():.3e}$')
+                plt.text(rand_dot_prod_results.mean(), plt.ylim()[1]/2,
+                         f'Random: $\\mu={rand_dot_prod_results.mean():.3e}$,\n'
+                         f'$\\sigma={rand_dot_prod_results.var():.3e}$')
+                plt.show()
+            else:
+                # Mirco-teleportation
+                plt.hist(np.array(dot_prod_results), color='b', bins=bins)
+                plt.title(f'Micro-teleportation - Sampling type: {sampling_type}, cob range: {cob}\n'
+                          f'{teleport_iterations} iterations')
+                plt.text(dot_prod_results.mean(), plt.ylim()[1]/2,
+                         f'Micro: $\\mu={dot_prod_results.mean():.3e}$,\n'
+                         f'$\\sigma={dot_prod_results.var():.3e}$')
+                plt.show()
 
-            plt.hist(np.array(rand_dot_prod_results), color='g')
-            plt.title(f'Random vector: Sampling type: {sampling_type}, cob range: {cob}')
-            plt.show()
+                # Random vector
+                plt.hist(np.array(rand_dot_prod_results), color='g', bins=bins)
+                plt.title(f'Random vector - Sampling type: {sampling_type}, cob range: {cob}\n'
+                          f'{teleport_iterations} iterations')
+                plt.text(rand_dot_prod_results.mean(), plt.ylim()[1]/2,
+                         f'Random: $\\mu={rand_dot_prod_results.mean():.3e}$,\n'
+                         f'$\\sigma={rand_dot_prod_results.var():.3e}$')
+                plt.show()
 
 if __name__ == '__main__':
     import torch.nn as nn
