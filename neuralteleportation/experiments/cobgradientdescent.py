cob fixes cob Gradient descent works!

neuralteleportation/experiments/cobgradientdescent.py
@@ -11,7 +11,6 @@ from neuralteleportation.layers.neuralteleportation import FlattenCOB
 from neuralteleportation.layers.neuron import LinearCOB
 from neuralteleportation.metrics import accuracy
 from neuralteleportation.neuralteleportationmodel import NeuralTeleportationModel
-from neuralteleportation.training import train
 
 torch.manual_seed(10)
 
@@ -73,7 +72,7 @@ Optimize the cob to find the 'target_cob' that produced the original teleportati
 optimizer = optim.Adam([cob], lr=1e-3)
 # scheduler = StepLR(optimizer, step_size=10000, gamma=0.1)
 
-for e in range(10000):
+for e in range(5000):
     # Reset the initial weights.
     model.set_weights(initial_weights)
 

neuralteleportation/experiments/coboptim.py
@@ -11,14 +11,15 @@ from neuralteleportation.layers.neuralteleportation import FlattenCOB
 from neuralteleportation.layers.neuron import LinearCOB
 from neuralteleportation.metrics import accuracy
 from neuralteleportation.neuralteleportationmodel import NeuralTeleportationModel
-from neuralteleportation.training import train, test
+from neuralteleportation.training.config import TrainingConfig, TrainingMetrics
 
 # torch.manual_seed(1234)
+from neuralteleportation.training.training import train
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-epochs = 0
-batch_size = 128
-metrics = [accuracy]
-loss = nn.CrossEntropyLoss()
+config = TrainingConfig()
+config.epochs = 0
+metrics = TrainingMetrics(nn.CrossEntropyLoss(), [accuracy])
 
 mnist_train = MNIST('/tmp', train=True, download=True, transform=transforms.ToTensor())
 mnist_test = MNIST('/tmp', train=False, download=True, transform=transforms.ToTensor())
@@ -41,13 +42,11 @@ sample_input_shape = (1, 1, 28, 28)
 
 model1 = Model().to(device)
 model1 = NeuralTeleportationModel(network=model1, input_shape=sample_input_shape)
-train(model1, criterion=loss, train_dataset=mnist_train, val_dataset=mnist_test, metrics=metrics,
-      epochs=epochs, device=device, batch_size=batch_size)
+train(model1, train_dataset=mnist_train, metrics=metrics, config=config, val_dataset=mnist_test)
 
 model2 = Model().to(device)
 model2 = NeuralTeleportationModel(network=model2, input_shape=sample_input_shape)
-train(model2, criterion=loss, train_dataset=mnist_train, val_dataset=mnist_test, metrics=metrics,
-      epochs=epochs, device=device, batch_size=batch_size)
+train(model2, train_dataset=mnist_train, metrics=metrics, config=config, val_dataset=mnist_test)
 
 print("Before")
 w1 = model1.get_weights()

neuralteleportation/neuralteleportationmodel.py
@@ -49,7 +49,7 @@ class NeuralTeleportationModel(nn.Module):
         for i in range(len(neuron_layers) - 1):
             size += neuron_layers[i].out_features
 
-        return get_random_cob(range=cob_range, size=size, requires_grad=True)
+        return get_random_cob(range_cob=cob_range, size=size, requires_grad=True)
 
     def get_random_change_of_basis(self, basis_range=0.5, sampling_type='usual'):
         """
@@ -312,8 +312,8 @@ class NeuralTeleportationModel(nn.Module):
             t = layers[i].calculate_cob(initial_weights[i], target_weights[i], cob[-1])
             cob.append(t)
 
-        print("Last layer")
-        print(layers[-2].out_features)
+        # print("Last layer")
+        # print(layers[-2].out_features)
         t = []
         for i in range(layers[-2].out_features):
             w0 = initial_weights[-2][i, :]
@@ -323,31 +323,31 @@ class NeuralTeleportationModel(nn.Module):
             t0 = cob[-1]
             t2 = layers[-1].get_output_cob()
 
-            print(w0.shape)
-            print(w1.shape)
-            print(w0_hat.shape)
-            print(w1_hat.shape)
-            print(t0.shape)
-            print(t2.shape)
+            # print(w0.shape)
+            # print(w1.shape)
+            # print(w0_hat.shape)
+            # print(w1_hat.shape)
+            # print(t0.shape)
+            # print(t2.shape)
 
             ti = torch.tensor(1.1)
 
             eta = 0.001
 
             for _ in range(20):
-                print((w0 / t0).dot(w0 / t0))
-                print((w0 / t0).dot(w0_hat))
-                print((w1 * t2).dot(w1 * t2))
-                print((w1 * t2).dot(w1_hat))
+                # print((w0 / t0).dot(w0 / t0))
+                # print((w0 / t0).dot(w0_hat))
+                # print((w1 * t2).dot(w1 * t2))
+                # print((w1 * t2).dot(w1_hat))
                 grad = (2 * ti * (w0 / t0).dot(w0 / t0) -
                         2 * (w0 / t0).dot(w0_hat) -
                         2 * torch.pow(ti, -3) * (w1 * t2).dot(w1 * t2) +
                         2 * torch.pow(ti, -2) * (w1 * t2).dot(w1_hat))
-                print("ti: ", ti)
-                print("grad: ", grad)
+                # print("ti: ", ti)
+                # print("grad: ", grad)
                 ti = ti - eta * grad
 
-            print("final ti: ", ti)
+            # print("final ti: ", ti)
             t.append(ti)
 
         cob.append(torch.tensor(t))

tests/model_test.py
@@ -18,13 +18,13 @@ def test_set_weights(network: nn.Module, input_shape: Tuple = (1, 1, 28, 28), mo
     model_name = model_name or network.__class__.__name__
 
     model = NeuralTeleportationModel(network, input_shape)
-    w1 = model.get_weights().detach().numpy()
+    w1 = model.get_weights()
 
     model.reset_weights()
     model.set_weights(w1)
-    w2 = model.get_weights().detach().numpy()
+    w2 = model.get_weights()
 
-    assert np.allclose(w1, w2)
+    assert np.allclose(w1.detach().numpy(), w2.detach().numpy())
     print("Weights set successfully for " + model_name + " model.")
 
 
@@ -135,8 +135,8 @@ def test_calculate_cob(network, model_name, input_shape=(1, 1, 28, 28), noise=Fa
     calculated_cob = model.calculate_cob(w1, w2)
 
     if verbose:
-        print("Cob: ", cob.flatten()[:10])
-        print("Calculated cob: ", calculated_cob.flatten()[:10])
+        print("Cob: ", cob.flatten())
+        print("Calculated cob: ", calculated_cob.flatten())
 
     assert np.allclose(cob, calculated_cob)
 
@@ -191,6 +191,8 @@ if __name__ == '__main__':
         Flatten(),
         nn.Linear(784, 128),
         nn.ReLU(),
+        nn.Linear(128, 128),
+        nn.ReLU(),
         nn.Linear(128, 10)
     )
 
@@ -208,5 +210,4 @@ if __name__ == '__main__':
     test_set_cob(network=mlp_model, model_name="MLP")
     test_set_cob(network=cnn_model, model_name="Convolutional")
 
-    test_calculate_cob(network=mlp_model, model_name="MLP")
-    test_calculate_cob(network=cnn_model, model_name="Convolutional")
+    test_calculate_cob(network=mlp_model, model_name="MLP", verbose=True)