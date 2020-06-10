1dc8206ba7e6682041e19daad609a2553423131b
Marco Armenta <marco.antonio.armenta.armenta@usherbrooke.ca>
2020-06-10 15:09:13 -0400

    Cleaned code for PR

--- neuralteleportation/experiments/statistics_teleportations.py
+++ neuralteleportation/experiments/statistics_teleportations.py
@@ -1,25 +1,38 @@
 from neuralteleportation.neuralteleportationmodel import NeuralTeleportationModel
-from neuralteleportation.training.config import TrainingMetrics, TrainingConfig
-from neuralteleportation.training.training import train
 from neuralteleportation.models.model_zoo.vggcob import vgg16COB
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+from numpy import save
 
 
-def plot_angle_teleported_gradient(model, loss_func, input_shape = (4, 3, 32, 32), n_iter=1000):
+def plot_angle_teleported_gradient(model, loss_func, input_shape=(4, 3, 32, 32), n_iter=200):
+    """
+    This method plots a histogram of the angles between the gradient of the network model and the gradients of
+     n_iter teleportations of it.
+
+    Args:
+        model : NeuralTeleportationModel to test.
+
+        input_shape : The shape of the input.
+
+        loss_func : Loss function to compute gradients.
+
+        n_iter : Number of teleportations computed for each cob_range.
+    """
     x = torch.rand(input_shape, dtype=torch.float)
     y = torch.randint(low=0, high=9, size=(4,))
+
     original_weights = model.get_weights()
-    original_grad = model.get_grad(x, y, loss_func, zero_grad=False)
+    grad1 = model.get_grad(x, y, loss_func, zero_grad=False)
     angle_results = []
 
     for _ in range(n_iter):
         model.set_weights(original_weights)
-        model.random_teleport(cob_range=50, sampling_type='positive')
-        teleported_grad = model.get_grad(x, y, loss_func, zero_grad=False)
-        dot_prod = np.longfloat(np.dot(original_grad, teleported_grad) /
-                                (np.linalg.norm(original_grad) * np.linalg.norm(teleported_grad)))
+        model.random_teleport(cob_range=100, sampling_type='positive')
+        grad2 = model.get_grad(x, y, loss_func, zero_grad=False)
+        dot_prod = np.longfloat(np.dot(grad1, grad2) /
+                                (np.linalg.norm(grad1) * np.linalg.norm(grad2)))
         angle = np.degrees(np.arccos(dot_prod))
         angle_results.append(angle)
 
@@ -33,78 +46,82 @@ def plot_angle_teleported_gradient(model, loss_func, input_shape = (4, 3, 32, 32
     width = bin_boundary[1] - bin_boundary[0]
     bin_height = bin_height / float(max(bin_height))
     plt.bar(bin_boundary[:-1], bin_height, width=np.maximum(width, 0.05))
-    plt.title('Angle of gradients between teleportations')
-    plt.ylabel('Count')
+    plt.title('Histogram of angles of gradient with multiple teleported gradients')
     plt.xlabel('Angle')
     plt.xlim(x_min, x_max)
     plt.show()
 
 
-def plot_magnitude_teleported_gradient(model, loss_func, input_shape = (4, 3, 32, 32), n_iter=20):
+def plot_difference_teleported_gradients(model, loss_func, input_shape=(4, 3, 32, 32), n_iter=20, save_to_files=False):
+    """
+    This method plots the difference of the gradient of model and the gradient of a teleportation, by increasing the
+    cob_range from 0.1 to 0.9 in n_iter iterations for usual cob_sampling. Each gradient is normalized by the norm of
+    the weights producing the corresponding gradient.
+
+    Args:
+        model : NeuralTeleportationModel to test.
+
+        input_shape : The shape of the input.
+
+        loss_func : Loss function to compute gradients.
+
+        n_iter : Number of teleportations computed for each cob_range.
+
+        save_to_files : Flag to decide when to save results for the plot to a file.
+    """
     x = torch.rand(input_shape, dtype=torch.float)
     y = torch.randint(low=0, high=9, size=(4,))
+
     original_weights = model.get_weights().detach().numpy()
     original_grad = model.get_grad(x, y, loss_func, zero_grad=False).numpy()
     original_grad = original_grad / np.linalg.norm(original_weights)
+
     differences = []
     variance = []
-    center = 0.2
-    xaxis = []
+    x_axis = np.linspace(0.1, 0.9, n_iter)
 
     for i in range(n_iter):
-        center += 0.05
-        xaxis.append(center)
         to_compute_mean = []
-        for j in range(10):
+        for j in range(100):
             model.set_weights(original_weights)
-            model.random_teleport(cob_range=0.001, sampling_type='positive_centered', center=center)
+            model.random_teleport(cob_range=x_axis[i], sampling_type='usual')
 
-            teleported_grad = model.get_grad(x, y, loss_func, zero_grad=False).numpy()
             teleported_weights = model.get_weights().detach().numpy()
+            teleported_grad = model.get_grad(x, y, loss_func, zero_grad=False).numpy()
             teleported_grad = teleported_grad / np.linalg.norm(teleported_weights)
-            diff = abs(np.linalg.norm(original_grad)-np.linalg.norm(teleported_grad))
 
+            diff = abs(np.linalg.norm(original_grad)-np.linalg.norm(teleported_grad))
             to_compute_mean.append(diff)
 
-        variance.append(np.var(to_compute_mean))
+        variance.append(np.std(to_compute_mean))
         differences.append(np.mean(to_compute_mean))
 
     variance = np.array(variance)
     differences = np.array(differences)
-    xaxis = np.array(xaxis)
-    plt.errorbar(xaxis, differences, yerr=variance)
-    plt.plot(xaxis, differences)
+    x_axis = np.array(x_axis)
+
+    if save_to_files:
+        save('variance.npy', variance)
+        save('differences.npy', differences)
+        save('x_axis.npy', x_axis)
+
+    plt.errorbar(x_axis, differences, yerr=variance)
+    plt.plot(x_axis, differences)
+
     plt.title('Difference of normalized magnitude between teleportations of the gradient')
-    #plt.xlim(0, n_iter)
     plt.ylabel('| ||Grad||/||W|| - ||Tel.Grad||/||Tel.W|| |')
-    plt.xlabel('cob_range in (x-0.1,x+0.1)')
+    plt.xlabel('cob_range')
     plt.show()
 
 
 if __name__ == '__main__':
-    from torchvision.datasets import CIFAR10
-    import torchvision.transforms as transforms
-    from neuralteleportation.metrics import accuracy
     import torch.nn as nn
-    from torch.utils.data import DataLoader
 
-    input_shape = (32, 3, 32, 32)
-    loss_func = nn.CrossEntropyLoss()
+    input_shape = (4, 3, 32, 32)
+    loss = nn.CrossEntropyLoss()
 
-    #CIFAR10_train = CIFAR10('/tmp', train=True, download=True, transform=transforms.ToTensor())
-    #CIFAR10_val = CIFAR10('/tmp', train=False, download=True, transform=transforms.ToTensor())
-    #CIFAR10_test = CIFAR10('/tmp', train=False, download=True, transform=transforms.ToTensor())
+    network = vgg16COB(pretrained=False, num_classes=10, input_channels=3)
+    network = NeuralTeleportationModel(network, input_shape=input_shape)
 
-    model = vgg16COB(pretrained=False, num_classes=10, input_channels=3)
-    model = NeuralTeleportationModel(model, input_shape=input_shape)
-
-    #config = TrainingConfig(device='cuda', epochs=5, lr=0.1)
-    #metrics = TrainingMetrics(loss_func, [accuracy])
-
-    #train_loader = DataLoader(CIFAR10_train, batch_size=config.batch_size)
-
-    #train(model.to(device='cuda'), train_dataset=CIFAR10_train, metrics=metrics, config=config,
-    #      val_dataset=CIFAR10_val)
-    plot_magnitude_teleported_gradient(model, loss_func)
-    #plot_angle_teleported_gradient(model, loss_func)
-
+    plot_difference_teleported_gradients(network, loss)
+    plot_angle_teleported_gradient(network, loss)
