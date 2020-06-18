a760b5ea36e2daa092dc33953c3a76d91284ce75
Philippe Spino <philippe.spino@usherbrooke.ca>
2020-06-18 13:11:55 -0400

    Made some changes from PR Move multiple_model_comparison logic to plot_hft5_training_curves script

--- neuralteleportation/experiments/multiple_model_training_comparision.py
+++ neuralteleportation/experiments/multiple_model_training_comparision.py
@@ -1,10 +1,7 @@
 import pathlib
-import glob
-import h5py
-import numpy as np
-import matplotlib.pyplot as plt
-
 import argparse
+
+from neuralteleportation.utils.plot_hft5_training_curves import plot_training_curves
 
 
 def argument_parser():
@@ -25,41 +22,6 @@ if __name__ == '__main__':
     args = argument_parser()
     path = pathlib.Path(args.folder) if args.folder else pathlib.Path().absolute() / "results/"
 
-    fig = plt.figure()
+    fig = plot_training_curves(path, args.show_err, args.std_err, args.show_grid, show=False)
 
-    for fpath in glob.glob(str(path / "*.h5"), recursive=True):
-        splits = pathlib.Path(fpath).name.split('_')
-
-        # This preliminary check to not read files that does not fit
-        # the format from teleportation_train_comparison script
-        if len(splits) != 8:
-            continue
-        name = splits[6]
-
-        f = h5py.File(fpath, "r")
-        for k in f.keys():
-            data = np.array(f[k])
-            assert data.ndim == 2, "The open file does not contain the right data format or is "
-            x = np.arange(data.shape[1])
-            mean = data.mean(axis=0)
-            if args.std_err:
-                err = data.std(axis=0)
-            else:
-                err = -np.amin(data, axis=0) + mean, np.amax(data, axis=0) - mean
-            label = name + "_" + k
-            if args.show_err:
-                plt.errorbar(x, mean, yerr=err, fmt='-o', ms=3, label=label, capsize=5.0)
-            else:
-                plt.plot(mean, '-o', ms=3, label=label, )
-        f.close()
-
-    assert len(fig.get_axes()) > 0, "Nothing was drawn onto the graph! Verify the folder used for this script!"
-
-    plt.title("Neural Teleportation Model Training Comparison")
-    plt.xlabel("Epochs")
-    plt.ylabel("Accuracy")
-    if args.show_grid:
-        plt.grid()
-    plt.legend()
-
-    plt.show()
+    fig.show()
