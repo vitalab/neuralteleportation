c08c45a053a7339d211fc18b2240a0c4a27aca27
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2020-07-03 17:53:42 -0400

    Changed `optim_teleporting_gd` to run on hyperparameter grid by default

--- neuralteleportation/experiments/optim_teleporting_gd.py
+++ neuralteleportation/experiments/optim_teleporting_gd.py
@@ -1,7 +1,7 @@
 from neuralteleportation.changeofbasisutils import get_available_cob_sampling_types
+from neuralteleportation.utils.logger import VisdomLogger
 
 if __name__ == '__main__':
-    import argparse
     import operator
 
     from torch import nn
@@ -11,31 +11,36 @@ if __name__ == '__main__':
     from neuralteleportation.training.optim_score.training import train
     from neuralteleportation.training.optim_score.config import TeleportationTrainingConfig
     from neuralteleportation.training.experiment_setup import get_cifar10_models, get_cifar10_datasets
-    from neuralteleportation.training.experiment_run import run_single_output_training
+    from neuralteleportation.training.experiment_run import run_model_training
     from neuralteleportation.utils.model_eval import weighted_grad_norm, loss_lookahead_diff
-
-    parser = argparse.ArgumentParser(description="Network optimization experiment where gradient descent jumps between "
-                                                 "teleportations, based on a criterion evaluating the gradient of "
-                                                 "multiple teleportations")
-    parser.add_argument("--cob_sampling", type=str, choices=get_available_cob_sampling_types(),
-                        default='within_landscape',
-                        help="Sampling algorithm to use when generating a change of basis")
-    parser.add_argument("--cob_range", type=float, default=0.5)
-    parser.add_argument("--optim", type=str, choices=['grad_norm', 'lookahead'], default='grad_norm',
-                        help="Criterion used to select the optimal teleportation to train")
-    args = parser.parse_args()
 
     metrics = TrainingMetrics(nn.CrossEntropyLoss(), [accuracy])
 
-    if args.optim == 'grad_norm':
-        comparison_metric = (weighted_grad_norm, operator.gt)
-    else:  # args.optim == 'lookahead':
-        comparison_metric = (loss_lookahead_diff, operator.gt)
-
     # Run on CIFAR10
     cifar10_train, cifar10_val, cifar10_test = get_cifar10_datasets()
-    config = TeleportationTrainingConfig(input_shape=(3, 32, 32), device='cuda',
-                                         cob_range=args.cob_range, cob_sampling=args.cob_sampling,
-                                         comparison_metric=comparison_metric)
-    run_single_output_training(train, get_cifar10_models(device='cuda'), config, metrics,
-                               cifar10_train, cifar10_test, val_set=cifar10_val)
+    cob_ranges = [0.7, 1.2]
+    cob_samplings = get_available_cob_sampling_types()
+    teleport_every_n_epochs = [1, 2, 5, 10]
+    for sampling_type in cob_samplings:
+        for cob_range in cob_ranges:
+            for n in teleport_every_n_epochs:
+                for comparison_metric in [(weighted_grad_norm, operator.gt),
+                                          (loss_lookahead_diff, operator.gt)]:
+                    for model in get_cifar10_models(device='cuda'):
+                        env_name = "{}_{}_optim_teleport_{}_{}_every_{}".format(model.__class__.__name__,
+                                                                                comparison_metric[0].__name__,
+                                                                                sampling_type, cob_range, n)
+                        print("Starting: ", env_name)
+                        config = TeleportationTrainingConfig(
+                            input_shape=(3, 32, 32),
+                            device='cuda',
+                            cob_range=cob_range,
+                            cob_sampling=sampling_type,
+                            teleport_every_n_epochs=n,
+                            epochs=20,
+                            exp_logger=VisdomLogger(env=env_name),
+                            comparison_metric=comparison_metric
+                        )
+                        run_model_training(train, model,
+                                           config, metrics,
+                                           cifar10_train, cifar10_test, val_set=cifar10_val)
