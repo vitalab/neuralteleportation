49b7b38756cb01ba5e59ad79203dbfa23c7a8ee0
Philippe Spino <philippe.spino@usherbrooke.ca>
2020-07-08 16:04:29 -0400

    improved the output and added loss and acc avg computation on multiple runs

--- tests/teleportation_coequality_test.py
+++ tests/teleportation_coequality_test.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.nn as nn
 import numpy as np
@@ -16,64 +17,109 @@ from neuralteleportation.metrics import accuracy
 class Net4(nn.Module):
     def __init__(self):
         super(Net4, self).__init__()
-        self.conv1 = Conv2dCOB(1, 2, 3)
+        self.conv1 = Conv2dCOB(1, 64, 3)
         self.relu1 = ReLUCOB()
-        self.bn1 = BatchNorm2dCOB(2)
+        self.bn1 = BatchNorm2dCOB(64)
         self.flatten = FlattenCOB()
-        self.lin1 = LinearCOB(1352, 10)
+        self.lin1 = LinearCOB(43264, 10)
 
     def forward(self, x):
-        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.flatten(x)
+        x = self.lin1(x)
+        return x
 
-        return self.lin1(self.flatten(x))
+
+def argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", type=int, default=2)
+
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     trainset, _, testset = get_mnist_datasets()
 
+    args = argument_parser()
 
     metric = TrainingMetrics(
         criterion=torch.nn.CrossEntropyLoss(),
         metrics=[accuracy]
     )
     config = TrainingConfig(
-        batch_size=8192,
+        batch_size=32,
         epochs=1,
         device=device
     )
     model = NeuralTeleportationModel(Net4(), input_shape=(config.batch_size, 1, 28, 28)).to(device)
     # train(model, trainset, metrics=metric, config=config)
+    loss_avg = []
+    acc_avg = []
 
     print("==========================================")
-    res = test(model, trainset, metric, config)
-    loss1, acc1 = res['loss'], res['accuracy']
+    print("===========Testing w/o set/get============")
+    print("==========================================")
+    print()
+    print("==========================================")
+    for _ in range(args.t):
+        res = test(model, testset, metric, config)
+        loss1, acc1 = res['loss'], res['accuracy']
 
-    model.random_teleport()
-    res = test(model, trainset, metric, config)
-    loss2, acc2 = res['loss'], res['accuracy']
+        model.random_teleport()
+        res = test(model, testset, metric, config)
+        loss2, acc2 = res['loss'], res['accuracy']
+
+        loss_avg.append(np.abs(loss1 - loss2))
+        acc_avg.append(np.abs(acc1 - acc2))
+
+        print("==========================================")
+        print("Loss and accuracy diff without set/get was")
+        print("Loss diff was: {:.6e}".format(np.abs(loss1 - loss2)))
+        print("Acc diff was: {:.6e}".format(np.abs(acc1 - acc2)))
+        print("==========================================")
 
     print("==========================================")
     print("Loss and accuracy diff without set/get was")
-    print("Loss diff was: {:.6e}".format(np.abs(loss1 - loss2)))
-    print("Acc diff was: {:.6e}".format(np.abs(acc1 - acc2)))
+    print("Loss diff was: {:.6e}".format(np.mean(loss_avg)))
+    print("Acc diff was: {:.6e}".format(np.mean(acc_avg)))
     print("==========================================")
 
-    # model = NeuralTeleportationModel(Net4(), input_shape=(config.batch_size, 1, 28, 28)).to(device)
-    w_o = model.get_weights()
-    model.random_teleport()
-    w_t = model.get_weights()
+    print()
+    print("==========================================")
+    print("===========Testing with set/get===========")
+    print("==========================================")
+    print()
+    print("==========================================")
+    loss_avg = []
+    acc_avg = []
+    for _ in range(args.t):
+        model = NeuralTeleportationModel(Net4(), input_shape=(config.batch_size, 1, 28, 28)).to(device)
+        w_o = model.get_weights()
+        model.random_teleport()
+        w_t = model.get_weights()
 
-    model.set_weights(w_o)
-    res = test(model, trainset, metric, config)
-    loss1, acc1 = res['loss'], res['accuracy']
+        model.set_weights(w_o)
+        res = test(model, testset, metric, config)
+        loss1, acc1 = res['loss'], res['accuracy']
 
-    model.set_weights(w_t)
-    res = test(model, trainset, metric, config)
-    loss2, acc2 = res['loss'], res['accuracy']
+        model.set_weights(w_t)
+        res = test(model, testset, metric, config)
+        loss2, acc2 = res['loss'], res['accuracy']
+
+        loss_avg.append(np.abs(loss1 - loss2))
+        acc_avg.append(np.abs(acc1 - acc2))
+
+        print("==========================================")
+        print("Loss and accuracy diff with set/get was")
+        print("Loss diff was: {:.6e}".format(np.abs(loss1 - loss2)))
+        print("Acc diff was: {:.6e}".format(np.abs(acc1 - acc2)))
+        print("==========================================")
 
     print("==========================================")
-    print("Loss and accuracy diff with set/get was")
-    print("Loss diff was: {:.6e}".format(np.abs(loss1 - loss2)))
-    print("Acc diff was: {:.6e}".format(np.abs(acc1 - acc2)))
+    print("Loss and accuracy diff without set/get was")
+    print("Loss diff was: {:.6e}".format(np.mean(loss_avg)))
+    print("Acc diff was: {:.6e}".format(np.mean(acc_avg)))
     print("==========================================")
