a85f74469b67ed2766cfee6489065cfb1cadcbed
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2020-07-23 10:44:36 -0400

    Fixed positional arguments provided by name

--- neuralteleportation/experiments/submit_teleport_training.sh
+++ neuralteleportation/experiments/submit_teleport_training.sh
@@ -62,5 +62,5 @@ compute_node_data_dir="$SLURM_TMPDIR"/data
 rsync -a "$dataset_shared_dir" "$compute_node_data_dir"
 
 # Run task
-python neuralteleportation/experiments/teleport_training.py --config "$experiment_config_file" \
+python neuralteleportation/experiments/teleport_training.py "$experiment_config_file" \
   --data_root_dir "$compute_node_data_dir"
