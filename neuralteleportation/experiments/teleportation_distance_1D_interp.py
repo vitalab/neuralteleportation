91e9476d57c628c3d4a32a901ef564ed4c006ad1
Philippe Spino <philippe.spino@usherbrooke.ca>
2020-07-31 08:30:06 -0400

    added save/load to 1D interpolation experiment.

--- neuralteleportation/experiments/teleportation_distance_1D_interp.py
+++ neuralteleportation/experiments/teleportation_distance_1D_interp.py
@@ -4,15 +4,17 @@ import torch
 import torch.nn as nn
 from torch.cuda import is_available as cuda_avail
 
-from neuralteleportation.training.training import train
+from neuralteleportation.metrics import accuracy
+from neuralteleportation.training.training import train, test
 from neuralteleportation.training.experiment_setup import get_dataset_subsets, get_model, get_model_names
 from neuralteleportation.training.config import TrainingMetrics
 from neuralteleportation.losslandscape.losslandscape import LandscapeConfig, generate_1D_linear_interp, plot_interp
-from neuralteleportation.metrics import accuracy
 
 
 def argument_parser():
     parser = argparse.ArgumentParser()
+
+    """Hyper Parameters"""
     parser.add_argument("--epochs", "-e", type=int, default=10,
                         help="How many epochs should the network train in total")
     parser.add_argument("--lr", type=float, default=1e-3,
@@ -26,11 +28,21 @@ def argument_parser():
     parser.add_argument("--cob_sampling", type=str, default="within_landscape",
                         choices=['within_landscape', 'change_landscape', 'positive', 'negative', 'centered'],
                         help="Defines the type of sampling used for the COB. It must be a valide mix with cob_range")
+
+    """Linear Interpolation Config"""
     parser.add_argument("--x", nargs=3, type=float, default=[-0.5, 1.5, 101],
                         help="Defines the precision of the alpha")
+
+    """Models Configuration"""
     parser.add_argument("--train", action="store_true", default=False,
                         help="Whether or not the model should train before teleportation.")
     parser.add_argument("--model", type=str, default="resnet18COB", choices=get_model_names())
+    parser.add_argument("--save_model", action="store_true", default=False,
+                        help="Enable saving the model after training")
+    parser.add_argument("--save_path", type=str, default='/tmp/model.pt',
+                        help="")
+    parser.add_argument("--load_model", action="store_true", default=False)
+    parser.add_argument("--load_path", type=str, default='/tmp/model.pt')
 
     return parser.parse_args()
 
@@ -41,8 +53,6 @@ if __name__ == '__main__':
     device = 'cuda' if cuda_avail() else 'cpu'
 
     trainset, valset, testset = get_dataset_subsets("cifar10")
-
-    model = get_model("cifar10", args.model, device=device)
     metric = TrainingMetrics(
         criterion=nn.CrossEntropyLoss(),
         metrics=[accuracy]
@@ -56,8 +66,21 @@ if __name__ == '__main__':
         teleport_at=[args.epochs],
         device=device
     )
-    if args.train:
-        train(model, trainset, metric, config)
+
+    model = get_model("cifar10", args.model, device=device)
+    if args.load_model:
+        model.load_state_dict(torch.load(args.load_path))
+        res = test(model, trainset, metric, config)
+        print("Scored {:.4f} acc on trainset".format(res['accuracy']))
+        res = test(model, valset, metric, config)
+        print("Scored {:.4f} acc on valset".format(res['accuracy']))
+    else:
+        if args.train:
+            train(model, trainset, metric, config)
+            test(model, testset, metric, config)
+        if args.save_model:
+            torch.save(model.state_dict(), args.save_path)
+
     a = torch.linspace(args.x[0], args.x[1], int(args.x[2]))
     param_o = model.get_params()
     model.random_teleport(args.cob_range, args.cob_sampling)
