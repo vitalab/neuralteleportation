239c75499dce26aff0c9df678b2f4eb563fadc43
Nathan Painchaud <nathan.painchaud@usherbrooke.ca>
2020-09-16 13:52:59 -0400

    Added CLI configuration options

--- neuralteleportation/experiments/weights_histogram.py
+++ neuralteleportation/experiments/weights_histogram.py
@@ -5,20 +5,29 @@ import numpy as np
 import seaborn as sns
 
 from neuralteleportation.neuralteleportationmodel import NeuralTeleportationModel
-from neuralteleportation.training.experiment_setup import get_model
+from neuralteleportation.training.experiment_setup import get_model, get_model_names
 
 
-def argument_parser():
+def argument_parser() -> argparse.Namespace:
     """
         Simple argument parser for the experiment.
     """
     parser = argparse.ArgumentParser(description='Simple argument parser for the plot loss landscape experiment.')
+
+    # Model Parameters
+    parser.add_argument("--model", "-m", type=str, default="MLPCOB", choices=get_model_names())
+    parser.add_argument("--dataset", type=str, default="mnist", choices=["cifar10", "mnist"],
+                        help="Dataset used to initialize the shape of the network")
 
     # Hyper Parameters
     parser.add_argument("--cob_range", type=float, default=0.99, help='set the CoB range for the teleportation.')
     parser.add_argument("--cob_sampling", type=str, default="within_landscape", help="Sampling type for CoB.")
 
     # Plotting Parameters
+    parser.add_argument("--plot_mode", type=str, default="layerwise", choices=["layerwise", "modelwise"],
+                        help="Mode that determines how many histograms to produce: \n"
+                             "- layerwise: Plot an histogram of the weights for each layer \n"
+                             "- modelwise: Plot a single histogram of the weights that groups the whole network")
     parser.add_argument("--xlim", type=float, default=1.,
                         help="Bound (on either side of 0) between which to display the histogram. "
                              "Only used for the histogram of teleported weights")
@@ -26,8 +35,8 @@ def argument_parser():
     return parser.parse_args()
 
 
-def plot_model_weights_histogram(model: NeuralTeleportationModel, mode: str = "layerwise", title: str = None,
-                                 xlim: float = None):
+def plot_model_weights_histogram(model: NeuralTeleportationModel, mode: str, title: str = None, xlim: float = None) \
+        -> None:
 
     def _plot_1d_array_histogram(array: np.ndarray, title: str):
         axes = sns.histplot(array, kde=True)
@@ -58,14 +67,17 @@ def main():
     args = argument_parser()
 
     # Initialize the model
-    net = get_model("mnist", "MLPCOB", hidden_layers=(128,128,128,128))
+    model_kwargs = {}
+    if args.model == "MLPCOB":
+        model_kwargs["hidden_layers"] = (128,128,128,128)
+    net = get_model(args.dataset, args.model, **model_kwargs)
 
     # Plot an histogram of its weights
-    plot_model_weights_histogram(net, title="xavier_init")
+    plot_model_weights_histogram(net, args.plot_mode, title="xavier_init")
 
     # Teleport the model and plot an histogram of its teleported weights
     net.random_teleport(args.cob_range, args.cob_sampling)
-    plot_model_weights_histogram(net, title=f"{args.cob_range}_{args.cob_sampling}_cob", xlim=args.xlim)
+    plot_model_weights_histogram(net, args.plot_mode, title=f"{args.cob_range}_{args.cob_sampling}_cob", xlim=args.xlim)
 
 
 if __name__ == '__main__':
