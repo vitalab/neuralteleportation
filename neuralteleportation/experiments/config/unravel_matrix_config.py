29e39a95d258116b5e8dede75a37bb3de7300a68
Youssef Skandrani <youssef.skandrani@outlook.com>
2020-10-09 11:55:19 -0400

    Added initializers to unravel_matrix

--- neuralteleportation/experiments/config/unravel_matrix_config.py
+++ neuralteleportation/experiments/config/unravel_matrix_config.py
@@ -22,63 +22,65 @@ def unravel_matrix_config(config_path: Path, output_dir: Path) -> int:
 
         # models
         for model_name in config["models"]:
+            # initalizers
+            for initializer in config["initializers"]:
+                # optimizers
+                for optimizer_config in config["optimizers"]:
 
-            # optimizers
-            for optimizer_config in config["optimizers"]:
+                    # teleport configuration
+                    for teleport, teleport_config_kwargs in config["teleportations"].items():
 
-                # teleport configuration
-                for teleport, teleport_config_kwargs in config["teleportations"].items():
-
-                    # w/o teleport configuration
-                    if teleport == "no_teleport":
-                        # Ensure config collections are iterable, even if no config was defined
-                        # This is done to simplify the generation of the configuration matrix
-                        teleport_config_kwargs, teleport_mode_configs = {}, [("no_teleport", {})]
-
-                    # w/ teleport configuration
-                    else:  # teleport == "teleport"
-                        # Copy the config to play around with its content without affecting the config loaded in memory
-                        teleport_config_kwargs = teleport_config_kwargs.copy()
-
-                        teleport_mode_obj = teleport_config_kwargs.pop("mode")
-                        teleport_mode_configs = []
-                        for teleport_mode, teleport_mode_config_kwargs in teleport_mode_obj.items():
-
+                        # w/o teleport configuration
+                        if teleport == "no_teleport":
                             # Ensure config collections are iterable, even if no config was defined
                             # This is done to simplify the generation of the configuration matrix
-                            if teleport_mode_config_kwargs is None:
-                                teleport_mode_config_kwargs = {}
+                            teleport_config_kwargs, teleport_mode_configs = {}, [("no_teleport", {})]
 
-                            for teleport_mode_config_kwargs in dict_values_product(teleport_mode_config_kwargs):
-                                teleport_mode_configs.append((teleport_mode, teleport_mode_config_kwargs))
+                        # w/ teleport configuration
+                        else:  # teleport == "teleport"
+                            # Copy the config to play around with its content without affecting the config loaded in memory
+                            teleport_config_kwargs = teleport_config_kwargs.copy()
 
-                    # generate matrix of training configuration
-                    # (cartesian product of values for each training config kwarg)
-                    teleport_configs = dict_values_product(teleport_config_kwargs)
-                    config_matrix = itertools.product(teleport_configs, teleport_mode_configs)
+                            teleport_mode_obj = teleport_config_kwargs.pop("mode")
+                            teleport_mode_configs = []
+                            for teleport_mode, teleport_mode_config_kwargs in teleport_mode_obj.items():
 
-                    # Iterate over different possible training configurations
-                    for teleport_config_kwargs, (teleport_mode, teleport_mode_config_kwargs) in config_matrix:
-                        single_run_config_count += 1
-                        top_level_dict = {
-                            "datasets": [dataset_name],
-                            "models": [model_name],
-                            "optimizers": [optimizer_config],
-                            "training_params": config["training_params"],
-                            "teleportations": {teleport: None}
-                        }
-                        if teleport != "no_teleport":
-                            top_level_dict["teleportations"][teleport] = {
-                                "mode": {teleport_mode: listify_dict(teleport_mode_config_kwargs)},
-                                **listify_dict(teleport_config_kwargs)
+                                # Ensure config collections are iterable, even if no config was defined
+                                # This is done to simplify the generation of the configuration matrix
+                                if teleport_mode_config_kwargs is None:
+                                    teleport_mode_config_kwargs = {}
+
+                                for teleport_mode_config_kwargs in dict_values_product(teleport_mode_config_kwargs):
+                                    teleport_mode_configs.append((teleport_mode, teleport_mode_config_kwargs))
+
+                        # generate matrix of training configuration
+                        # (cartesian product of values for each training config kwarg)
+                        teleport_configs = dict_values_product(teleport_config_kwargs)
+                        config_matrix = itertools.product(teleport_configs, teleport_mode_configs)
+
+                        # Iterate over different possible training configurations
+                        for teleport_config_kwargs, (teleport_mode, teleport_mode_config_kwargs) in config_matrix:
+                            single_run_config_count += 1
+                            top_level_dict = {
+                                "datasets": [dataset_name],
+                                "models": [model_name],
+                                "optimizers": [optimizer_config],
+                                "initializers": [initializer],
+                                "training_params": config["training_params"],
+                                "teleportations": {teleport: None}
                             }
+                            if teleport != "no_teleport":
+                                top_level_dict["teleportations"][teleport] = {
+                                    "mode": {teleport_mode: listify_dict(teleport_mode_config_kwargs)},
+                                    **listify_dict(teleport_config_kwargs)
+                                }
 
-                        # Generate a unique name for the file, based on its content
-                        # file with the same content should have the same name
-                        config_hash = hashlib.sha1(json.dumps(top_level_dict, sort_keys=True).encode()).hexdigest()
+                            # Generate a unique name for the file, based on its content
+                            # file with the same content should have the same name
+                            config_hash = hashlib.sha1(json.dumps(top_level_dict, sort_keys=True).encode()).hexdigest()
 
-                        with open(str(output_dir.joinpath(f"{config_hash}.yml")), 'w') as single_run_config_file:
-                            yaml.dump(top_level_dict, single_run_config_file)
+                            with open(str(output_dir.joinpath(f"{config_hash}.yml")), 'w') as single_run_config_file:
+                                yaml.dump(top_level_dict, single_run_config_file)
 
     return single_run_config_count
 
